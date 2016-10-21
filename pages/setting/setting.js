3b978e989a17b9540e98aa56f6bf87c9813f0c18
KevinLin2012 <286146662@qq.com>
2016-10-21 15:59:04 +0800

    first commit

---
+++ pages/setting/setting.js
@@ -0,0 +1,15 @@
+var app = getApp()
+Page({
+  data: {
+    motto: '设置页面'
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      //url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+    
+  }
+})
